b48bb757d3a0140dbe668261a9659332a853a698
Justin Chee <justincy.chee@mail.utoronto.ca>
2020-03-23 14:28:02 -0400

    added modifications to combine_csv and divide_by_group

--- Code/divide_by_group.R
+++ Code/divide_by_group.R
@@ -11,21 +11,23 @@
 
 library(xlsx)
 library(ggplot2)
+library(data.table)
 
 # reading in and checking arguments from the command line
 args = commandArgs(trailingOnly = TRUE)
 
 if (length(args) > 2 | length(args) < 2) {
-  stop("2 arguments must be supplied: 
-       1. xlsx file containing metadata 
+  stop("2 arguments must be supplied:
+       1. xlsx file containing metadata
        2. path to directory containing csv files", call.=FALSE)
 } else if (!dir.exists(csv_dir)) {
   stop("Invalid csv directory", call.False)
 }
 
+# hard code directories so you can go through plots line by line 
 pheno <- "/external/rprshnas01/netdata_kcni/stlab/kristina_patchseq/Phenodata.xlsx"
 
-csv_dir <- "/external/rprshnas01/netdata_kcni/stlab/kristina_patchseq_processed/STAR_results/sortedbams/CSV/"
+csv_dir <- "/external/rprshnas01/netdata_kcni/stlab/kristina_patchseq_processed/STAR_results/pcrless_bams/InExCounts/"
 
 # loading in metadata
 meta <- read.xlsx(file = pheno, sheetIndex = 1)
@@ -51,45 +53,139 @@ nonflr_stack <- data.frame(sample = character(), condition = character(), value 
 ctrlwp_stack <- data.frame(sample = character(), condition = character(), value = integer())
 ctrlwop_stack <- data.frame(sample = character(), condition = character(), value = integer())
 
+# reading in combined count matrices
+combined_exons = read.csv(paste0(csv_dir,'combined_exons.csv'))
+combined_introns = read.csv(paste0(csv_dir,'combined_introns.csv'))
+combined_genes = read.csv(paste0(csv_dir,'combined_genes.csv'))
+
+# counting number of genes detected (counts > 0)
+gene_count_exon = colSums(combined_exons[,3:38] > 0, na.rm = T)
+gene_count_intron = colSums(combined_introns[,3:38] > 0, na.rm = T)
+gene_count_exons_introns = colSums(combined_genes[,3:38] > 0, na.rm = T)
+
+# separating gene counts based on location
+gene_count_exon_only <- colSums((combined_exons[,3:38] > 0 & 
+                                           combined_introns[,3:38] == 0),
+                                        na.rm = T)
+gene_count_intron_only <- colSums((combined_exons[,3:38] == 0 & 
+                                           combined_introns[,3:38] > 0),
+                                        na.rm = T)
+gene_count_exons_introns_only <- colSums((combined_exons[,3:38] > 0 & 
+                                           combined_introns[,3:38] > 0),
+                                        na.rm = T)
+
+# reformatting df to contain location and sample group info
+exon_only_df <- data.frame("gene_counts" = gene_count_exon_only, 
+                           "location" = rep("exons only", length(gene_count_exon_only)))
+setDT(exon_only_df, keep.rownames = "sample")
+intron_only_df <- data.frame("gene_counts" = gene_count_intron_only, 
+                           "location" = rep("introns only", length(gene_count_intron_only)))
+setDT(intron_only_df, keep.rownames = "sample")
+exon_intron_df <- data.frame("gene_counts" = gene_count_exons_introns_only, 
+                           "location" = rep("exons + introns", length(gene_count_exons_introns_only)))
+setDT(exon_intron_df, keep.rownames = "sample")
+
+# combined df so that stacked barplots can be generated
+gene_count_stack_df <- do.call("rbind", list(exon_only_df, intron_only_df, exon_intron_df))
+gene_count_stack_df$sample <- gsub("[^0-9]", "", gene_count_stack_df$sample)
+
+gene_count_stack_df$group <- "temp"
+
+# adding sample group info
+gene_count_stack_df[gene_count_stack_df$sample %in% flr$sample]$group <- "flr"
+gene_count_stack_df[gene_count_stack_df$sample %in% nonflr$sample]$group <- "nonflr"
+gene_count_stack_df[gene_count_stack_df$sample %in% ctrlwp$sample]$group <- "ctrlwp"
+gene_count_stack_df[gene_count_stack_df$sample %in% ctrlwop$sample]$group <- "ctrlwop"
+
+# plotting stacked gene counts
+jpeg(paste(csv_dir, "/", "gene_counts_stacked.jpg", sep = ""))
+
+ggplot(gene_count_stack_df, aes(fill=location, y=gene_counts, x=sample)) + 
+  geom_bar(position="stack", stat="identity") +
+  xlab("Sample") + ylab("Gene Count") + ggtitle("Allen Institute PCRless Gene Counts") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  facet_wrap( ~ group, scales = "free_x") + 
+  guides(fill=guide_legend(title="Read location"))
+
+dev.off()
+
+# plotting percent stacked gene counts
+jpeg(paste(csv_dir, "/", "gene_counts_percent_stacked.jpg", sep = ""))
+
+ggplot(gene_count_stack_df, aes(fill=location, y=gene_counts, x=sample)) + 
+  geom_bar(position="fill", stat="identity") +
+  xlab("Sample") + ylab("Gene Count") + ggtitle("Allen Institute PCRless Gene Counts") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  facet_wrap( ~ group, scales = "free_x") + 
+  guides(fill=guide_legend(title="Read location"))
+
+dev.off()
+
+# gene counts exons vs exons + introns
+gene_count_df <- data.frame("gene_count_exon_introns_only" = gene_count_exon_introns_only,
+                            "gene_count_exon_only" = gene_count_exon_only,
+                            "gene_count_intron_only" = gene_count_intron_only)
+
+gene_count_df %>% ggplot(aes(x = gene_count_exon_introns_only, y = gene_count_exon_only)) +
+  geom_point()
+
+read_count_exon_only = colSums(combined_exons[,3:38], na.rm = T)
+read_count_intron_only = colSums(combined_introns[,3:38], na.rm = T)
+read_count_exons_introns = colSums(combined_genes[,3:38], na.rm = T)
+
+meta_sample_df = data.frame('gene_count_exon_only' = gene_count_exon_only,
+           'gene_count_intron_only' = gene_count_intron_only,
+           'gene_count_exons_introns' = gene_count_exons_introns,
+           'read_count_exon_only' = read_count_exon_only,
+           'read_count_intron_only' = read_count_intron_only,
+           'read_count_exons_introns' = read_count_exons_introns
+           )
+meta_sample_df$sample = gsub("[^0-9]", "", rownames(meta_sample_df))
+rownames(meta_sample_df) = meta_sample_df$sample %>% make.names()
+
+meta_sample_df %>% ggplot(aes(x = gene_count_exons_introns, y = gene_count_exon_only)) +
+  geom_point()
+
+# $exons, $introns for Rsubread, $exon_count, $intron_count for Allen Institute
 for (i in csv_files) {
   
   if (gsub("[^0-9]", "", i) %in% flr$sample) {
     
-    count_matrix <- read.csv(file = paste(csv_dir, "/", i, sep = ""))
+    count_m <- read.csv(file = paste(csv_dir, "/", i, sep = ""))
     
     temp <- data.frame(sample = rep(gsub("[^0-9]", "", i), 2),
                        condition = c("exons", "introns"),
-                       value = c(sum(count_matrix$exons), sum(count_matrix$introns)))
+                       value = c(sum(count_m$exon_count), sum(count_m$intron_count)))
     
     flr_stack <- rbind(flr_stack, temp)
     
   } else if (gsub("[^0-9]", "", i) %in% nonflr$sample) {
-    
-    count_matrix <- read.csv(file = paste(csv_dir, "/", i, sep = ""))
+
+    count_m <- read.csv(file = paste(csv_dir, "/", i, sep = ""))
     
     temp <- data.frame(sample = rep(gsub("[^0-9]", "", i), 2),
                        condition = c("exons", "introns"),
-                       value = c(sum(count_matrix$exons), sum(count_matrix$introns)))
+                       value = c(sum(count_m$exon_count), sum(count_m$intron_count)))
     
     nonflr_stack <- rbind(nonflr_stack, temp)
     
   } else if (gsub("[^0-9]", "", i) %in% ctrlwp$sample) {
-    
-    count_matrix <- read.csv(file = paste(csv_dir, "/", i, sep = ""))
+
+    count_m <- read.csv(file = paste(csv_dir, "/", i, sep = ""))
     
     temp <- data.frame(sample = rep(gsub("[^0-9]", "", i), 2),
                        condition = c("exons", "introns"),
-                       value = c(sum(count_matrix$exons), sum(count_matrix$introns)))
+                       value = c(sum(count_m$exon_count), sum(count_m$intron_count)))
     
     ctrlwp_stack <- rbind(ctrlwp_stack, temp)
     
   } else if (gsub("[^0-9]", "", i) %in% ctrlwop$sample) {
-    
-    count_matrix <- read.csv(file = paste(csv_dir, "/", i, sep = ""))
+
+    count_m <- read.csv(file = paste(csv_dir, "/", i, sep = ""))
     
     temp <- data.frame(sample = rep(gsub("[^0-9]", "", i), 2),
                        condition = c("exons", "introns"),
-                       value = c(sum(count_matrix$exons), sum(count_matrix$introns)))
+                       value = c(sum(count_m$exon_count), sum(count_m$intron_count)))
     
     ctrlwop_stack <- rbind(ctrlwop_stack, temp)
     
@@ -103,15 +199,15 @@ ctrlwp_stack$group <- rep("ctrlwp", nrow(ctrlwp_stack))
 ctrlwop_stack$group <- rep("ctrlwop", nrow(ctrlwop_stack))
 
 group_stack <- do.call("rbind", list(flr_stack, nonflr_stack, ctrlwp_stack, ctrlwop_stack))
-print(group_stack)
 
 jpeg(paste(csv_dir, "/", "group_stacked.jpg", sep = ""))
 
 ggplot(group_stack, aes(fill = condition, y = value, x = sample)) +
   geom_bar(position = "stack", stat = "identity") +
-  xlab("Sample") + ylab("Counts") +
+  xlab("Sample") + ylab("Counts") + ggtitle("Allen Institute PCRless") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
-  facet_wrap( ~ group)
+  facet_wrap( ~ group, scales = "free_x") +
+  guides(fill=guide_legend(title="Read location"))
 
 dev.off()
 
@@ -119,11 +215,18 @@ jpeg(paste(csv_dir, "/", "group_percent_stacked.jpg", sep = ""))
 
 ggplot(group_stack, aes(fill = condition, y = value, x = sample)) +
   geom_bar(position = "fill", stat = "identity") +
-  xlab("Sample") + ylab("Counts") +
+  xlab("Sample") + ylab("Ratio") + ggtitle("Allen Institute PCRless") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
-  facet_wrap( ~ group)
+  facet_wrap( ~ group, scales = "free_x") + 
+  guides(fill=guide_legend(title="Read location"))
 
 dev.off()
+
+# meta_sample_df
+ggplot(merge(meta_sample_df, group_stack %>% dplyr::filter(condition == 'exons'), by = 'sample'), aes(x = sample, y = gene_count_exons_introns)) +
+  geom_bar(stat = "identity") + facet_wrap(~group, scales = "free_x") + ylab("Unique genes")
+
+# preliminary plotting examples
 
 # # plotting flr samples
 # jpeg(paste(csv_dir, "/", "flr_stacked.jpg", sep = ""))
